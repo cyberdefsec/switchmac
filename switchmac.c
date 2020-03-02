e98c008bfa91b0c820463009bf5c20939b706e67
AlekseiErilin1988 <43059387+AlekseiErilin1988@users.noreply.github.com>
2020-03-02 13:55:58 +0300

    Update switchmac.c

--- switchmac.c
+++ switchmac.c
@@ -20,29 +20,29 @@ void help(void){
 int main(int argc, char **argv){
     int opt = 0;
     opterr = false;
-    char macaddr[MAC_SIZE];
+    char mac_addr[MAC_SIZE];
     while((opt = getopt(argc, argv, "r:g:m:h")) != EOF){
         switch(opt){
             case 'r' :
-                if(getmac(macaddr, MAC_SIZE, argv[2]) != EOF && setmac_rand(argv[2]) != EOF){
-                    printf("Current mac address %s: %s\n", argv[2], macaddr);
-                    getmac(macaddr, MAC_SIZE, argv[2]);
-                    printf("New mac address %s: %s\n", argv[2], macaddr);
+                if(getmac(mac_addr, MAC_SIZE, argv[2]) != EOF && setmac_rand(argv[2]) != EOF){
+                    printf("Current mac address %s: %s\n", argv[2], mac_addr);
+                    getmac(mac_addr, MAC_SIZE, argv[2]);
+                    printf("New mac address %s: %s\n", argv[2], mac_addr);
                 }
                 else
                     perror("Set mac");
                 return 0;
             case 'm' :
-                if(getmac(macaddr, MAC_SIZE, argv[3]) != EOF && setmac(argv[2], argv[3]) != EOF){
-                    printf("Current mac address %s: %s\n", argv[3], macaddr);
+                if(getmac(mac_addr, MAC_SIZE, argv[3]) != EOF && setmac(argv[2], argv[3]) != EOF){
+                    printf("Current mac address %s: %s\n", argv[3], mac_addr);
                     printf("New mac address %s: %s\n", argv[3], argv[2]);
                 }
                 else
                     perror("Set mac");
                 return 0;
             case 'g' :
-                if(getmac(macaddr, MAC_SIZE, argv[2]) != EOF)
-                    printf("Current mac address %s: %s\n", argv[2], macaddr);
+                if(getmac(mac_addr, MAC_SIZE, argv[2]) != EOF)
+                    printf("Current mac address %s: %s\n", argv[2], mac_addr);
                 else
                     perror("Get mac");
                 return 0;
