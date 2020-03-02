b4d450472c8a21545d5d55e02a01b57d83f6a25e
AlekseiErilin1988 <43059387+AlekseiErilin1988@users.noreply.github.com>
2020-03-02 13:50:49 +0300

    Update getmac.c

--- getmac.c
+++ getmac.c
@@ -8,13 +8,13 @@
 #include <sys/ioctl.h>
 #include <net/if_arp.h>
 
-int getmac(char *macaddr, size_t sizebuf, char *dev){
+int getmac(char *mac_addr, size_t size_buf, char *dev){
     int sock;
     struct ifreq mac;
     if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP)) != EOF){
         snprintf(mac.ifr_name, IFNAMSIZ, "%s", dev);
         if(ioctl(sock, SIOCGIFHWADDR, &mac) != EOF){
-            snprintf(macaddr, sizebuf, "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X", (u_char)mac.ifr_hwaddr.sa_data[0],
+            snprintf(mac_addr, size_buf, "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X", (u_char)mac.ifr_hwaddr.sa_data[0],
                                                                         (u_char)mac.ifr_hwaddr.sa_data[1],
                                                                         (u_char)mac.ifr_hwaddr.sa_data[2],
                                                                         (u_char)mac.ifr_hwaddr.sa_data[3],
