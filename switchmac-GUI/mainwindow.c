f98ed949ce7853289884e2e2abff37df96c36167
aleks <aleks@PK>
2021-02-13 18:44:49 +0300

    update switchmac-GUI

--- switchmac-GUI/mainwindow.c
+++ switchmac-GUI/mainwindow.c
@@ -1,26 +1,18 @@
 #include "mainwindow.h"
 
-LISTDEVICE *get_interfaces(void){
-    LISTDEVICE *tmp = NULL;
-    if((tmp = (LISTDEVICE*)malloc(sizeof(LISTDEVICE))) != NULL){
-        strcpy(tmp->name, "aleks");
-        tmp->index = 0;
-        tmp->next = NULL;
-        return tmp;
-    }
-    return NULL;
-}
+static GtkWidget *list_interfaces = NULL;
 
 void main_window(){
     GtkWidget *win = NULL;
-    GtkWidget *list_interfaces = NULL;
     GtkWidget *btn_reload = NULL;
+    GtkWidget *btn_random = NULL;
+    GtkWidget *btn_change = NULL;
+    GtkWidget *check_on_rand = NULL;
     GtkWidget *vbox = NULL;
     GtkWidget *hbox_lab = NULL;
     GtkWidget *hbox_combo_btn = NULL;
     GtkWidget *lab_name_interface = NULL;
-    LISTDEVICE *list_device = NULL;
-    GtkWidget *btn_image = gtk_image_new_from_file("reload.png");
+
     win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(win), "Switchmac-GUI");
 
@@ -37,14 +29,14 @@ void main_window(){
     gtk_box_pack_start(GTK_BOX(vbox), hbox_combo_btn, false, false, 0);
 
     list_interfaces = gtk_combo_box_new_text();
-    list_device = get_interfaces();
-    gtk_combo_box_append_text(GTK_COMBO_BOX(list_interfaces), list_device->name);
-    gtk_combo_box_set_active(GTK_COMBO_BOX(list_interfaces), list_device->index);
+
     gtk_container_add(GTK_CONTAINER(hbox_combo_btn), list_interfaces);
 
     btn_reload = gtk_button_new();
-    gtk_button_set_image(GTK_BUTTON(btn_reload), btn_image);
     gtk_box_pack_start(GTK_BOX(hbox_combo_btn), btn_reload, false, false, 2);
+
+    check_on_rand = gtk_check_button_new_with_label("Random");
+    gtk_container_add(GTK_CONTAINER(vbox), check_on_rand);
 
     gtk_widget_show_all(win);
     g_signal_connect(G_OBJECT(win), "destroy", GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
--- switchmac-GUI/mainwindow.h
+++ switchmac-GUI/mainwindow.h
@@ -1,16 +1,6 @@
 #include <gtk/gtk.h>
 #include <stdbool.h>
 
-#define LEN_DEVICE 32
-
-typedef struct _LISTDEVICE{
-    guint index;
-    gchar name[LEN_DEVICE];
-    struct _LISTDEVICE *next;
-} LISTDEVICE;
-
 void main_window(void);
-
-LISTDEVICE *get_interfaces(void);
 
 
